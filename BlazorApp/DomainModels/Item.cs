3473a1ef5598ee96dcefd6b4c1c3547a0cdadf51
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-03-13 09:02:35 +0100

    PR test

---
+++ BlazorApp/DomainModels/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModels
+{
+    public abstract class Item : User
+    {
+        private int itemID;
+        public string gameName;
+        public int price;
+        public bool orderStatus;
+        public string condition;
+        public DateTime created;
+        public DateTime updated;
+        public string genre;
+        public string manufacture;
+        public bool addToFaverite;
+        private User userID;
+
+
+		void SetItem() { }
+        void AddItem() { }
+        void RemoveItem() { }
+        void ItemStatus() { }
+        void GetItem() { } 
+    }
+}
---
+++ BlazorApp/DomainModels/PC_Games.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain_Models
+{
+    public class PC_Game : Item
+    {
+        public int yearDeployed;
+        public List<string> pictures;
+        public string operatingSystem;
+    }
+}
