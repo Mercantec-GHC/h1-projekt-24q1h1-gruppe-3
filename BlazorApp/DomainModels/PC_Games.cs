35bed61351d62cb11a8313abea7447bc7c994585
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-03-19 12:19:04 +0100

    Implemented database

--- BlazorApp/DomainModels/PC_Games.cs
+++ BlazorApp/DomainModels/PC_Games.cs
@@ -7,8 +7,26 @@ namespace DomainModels
 {
     public class PC_Game : Item
     {
-        public int yearDeployed;
-        public List<string> pictures;
-        public string operatingSystem;
+        public string? operatingSystem { get; set; }
+        public DateTime yearDeployed { get; set; }
+
+        public PC_Game(int itemID, string gameName, int price, bool orderStatus, string condition, DateTime created, DateTime updated, string genre, string manufacture, bool addToFaverite, string description, string operatingSystem, DateTime yearDeployed)
+        {
+            // Initialiser felterne her
+            this.itemID = itemID;
+            this.gameName = gameName;
+            this.price = price;
+            this.orderStatus = orderStatus;
+            this.condition = condition;
+            this.created = created;
+            this.updated = updated;
+            this.genre = genre;
+            this.manufacture = manufacture;
+            this.addToFaverite = addToFaverite;
+            this.description = description;
+            this.operatingSystem = operatingSystem;
+            this.yearDeployed = yearDeployed;
+        }
     }
+
 }
