3a340dd812357e8ceb9dfc6baf1f4466ed6dc0ef
MdotKvist <Kvist339@gmail.com>
2024-03-27 08:48:33 +0100

    Updated sql script

--- SQL-Scripts/script.sql
+++ SQL-Scripts/script.sql
@@ -8,3 +8,11 @@ CREATE TABLE item (
     condition VARCHAR(255),
     description TEXT
 );
+
+CREATE TABLE users (id SERIAL PRIMARY KEY,  name text, phonenumber text, email text, city text);
+
+ALTER TABLE items ADD user_id INTEGER,
+ADD CONSTRAINT fk_user
+FOREIGN KEY(user_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
