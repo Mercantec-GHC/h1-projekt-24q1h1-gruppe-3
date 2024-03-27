33bcb63193eb5f12eba044ba569fe9eb20351665
StineDk <144903306+StineDk@users.noreply.github.com>
2024-03-27 15:00:00 +0100

    Update DatabaseService.cs

--- BlazorApp/Service/DatabaseService.cs
+++ BlazorApp/Service/DatabaseService.cs
@@ -308,5 +308,35 @@ namespace Service
             }
 
         }
+
+        public List<int> GetFavoritesByUserID(int id)
+        {
+            List<int> favorites = new List<int>();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = $@"SELECT array_to_string(favorites, ',') AS favorites 
+                      FROM users 
+                      WHERE id={id}";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            foreach (var item in reader["favorites"].ToString().Split(','))
+                            {
+                                favorites.Add(Convert.ToInt32(item));
+                            }
+                            //favorites = Convert.ToInt32(reader["favorites"].ToString()).ToList();
+                        }
+                        return favorites;
+                    }
+                }
+            }
+        }
     }
 }
